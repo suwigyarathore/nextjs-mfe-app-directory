47608ddf263b1fd6a4639d0d31f2b2e4609d7e34
Suwigya Rathore <s.rathore@quin.md>
2023-04-24 16:57:17 +0200

    reproduce repo

---
+++ apps/mfe-remote/next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+const remotes = isServer => {
+  const location = isServer ? 'ssr' : 'chunks';
+  return {
+    'mfe-remote': `mfe-remote@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'checkout',
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './sub-app-page': './src/app/sub-app/page.js'
+        },
+        remotes: remotes(options.isServer),
+        shared: {},
+        extraOptions:{
+          automaticAsyncBoundary: true
+        }
+      }),
+    );
+
+    return config;
+  },
+}
+
+module.exports = nextConfig
---
+++ apps/mfe-remote/src/app/sub-app/page.js
@@ -0,0 +1,19 @@
+async function getData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+  
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json();
+  }
+  
+  export default async function Page() {
+    const data = await getData();
+  
+    return <main></main>;
+  }
